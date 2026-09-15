984c7c56731270697a535f27a130e7d4c097303d
Lenoxlkz <1.98505191e+08+Lenoxlkz@users.noreply.github.com>
2026-09-15 07:03:29 +0000

    Remove files not present in updated archive

--- test-next6.js
+++
@@ -1,7 +0,0 @@
-import { MusicKit } from 'musicstream-sdk';
-const kit = new MusicKit();
-async function run() {
-  const res = await kit.getRelated('fJ9rUzIMcZQ');
-  console.log(JSON.stringify(res, null, 2).slice(0, 1000));
-}
-run();
